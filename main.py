59540d95a723d9f5b0b909e523ae0431fa1086ba
Flaty <78325079+Flaty@users.noreply.github.com>
2024-10-09 19:02:36 +0300

    Залил проект

---
+++ main.py
@@ -0,0 +1,71 @@
+import ptbot
+import os
+from pytimeparse import parse
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TG_TOKEN = os.getenv('TG_TOKEN')
+TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+bot = ptbot.Bot(TG_TOKEN)
+
+
+def render_progressbar(total,
+                       iteration,
+                       prefix='',
+                       suffix='',
+                       length=30,
+                       fill='█',
+                       zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+def notify_progress(secs_left, chat_id, message_id, message):
+    bot.update_message(
+        chat_id, message_id,
+        f'Осталось {secs_left} секун\n{render_progressbar(
+            parse(message),
+            parse(message) - secs_left
+        )}')
+
+
+def reply(chat_id, message):
+    message_id = bot.send_message(
+        chat_id,
+        'Запускаю таймер'
+    )
+    bot.create_timer(
+        parse(message),
+        choose,
+        chat_id=chat_id,
+        message=message
+    )
+    bot.create_countdown(
+        parse(message),
+        notify_progress,
+        message_id=message_id,
+        chat_id=chat_id,
+        message=message
+    )
+
+
+def choose(chat_id, message):
+    message = 'Время вышло'
+    bot.send_message(
+        chat_id,
+        message
+    )
+
+
+def main():
+    bot.reply_on_message(reply)
+    bot.run_bot()
+
+
+if __name__ == "__main__":
+    main()
